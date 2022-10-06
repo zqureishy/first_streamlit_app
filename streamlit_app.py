333f013161e8410a6fcdaa1fdc51572816472558
zqureishy <115059444+zqureishy@users.noreply.github.com>
2022-10-05 18:47:26 -0600

    Update streamlit_app.py
    added more text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & BlueBerry Oatmeal')
+streamlit.text('Kayle, Smoothie & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
