5bf844abad63b8885088631a8aa8164af187cd9c
zqureishy <115059444+zqureishy@users.noreply.github.com>
2022-11-22 13:47:24 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
-  streamlit.dataframe(my_data_rows)
+    streamlit.dataframe(my_data_rows)
   
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
