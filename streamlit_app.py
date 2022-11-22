1d32ee4e45c20b3a8b6289684740847968e500db
zqureishy <115059444+zqureishy@users.noreply.github.com>
2022-11-22 11:36:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-#from urllib.error import urlError
+from urllib.error import URLError
 
 #import streamlit
 streamlit.title('My Parents New Healthy Dinner')
